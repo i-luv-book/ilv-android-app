67503f0d679603e4c7b92212b624f2567e32afcb
김상익 <ksik518@naver.com>
2024-09-04 16:07:09 +0900

    feat : 동화 인트로 관련 Fragment, ViewModel 추가
    FairyTaleIntroFragment, FairyTaleIntroViewModel 추가
    동화 Response 관련 DTO 추가
    FairyTaleService의 suspend 함수에서 Call 대신 Response 사용

--- app/src/main/java/com/sangik/iluvbook/network/service/FairyTaleService.kt
+++ app/src/main/java/com/sangik/iluvbook/network/service/FairyTaleService.kt
@@ -2,15 +2,16 @@ package com.sangik.iluvbook.network.service
 
 import com.sangik.iluvbook.fairytale.model.dto.FairyTaleRequest
 import com.sangik.iluvbook.fairytale.model.dto.FairyTaleResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface FairyTaleService {
+    // 동화 생성
     @POST("fairytale/{difficulty}")
     suspend fun createFairyTale(
         @Path("difficulty") difficulty: String,
         @Body request: FairyTaleRequest
-    ): Call<FairyTaleResponse>
+    ): Response<FairyTaleResponse>
 }
