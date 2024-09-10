7063b6f57cd0ca2242f3adc9e20a9b5a8da2aaa0
김상익 <ksik518@naver.com>
2024-09-10 22:40:18 +0900

    feat : FairyTalePageAdapter 구현
    - FairyTaleDetailFragment의 ViewPager2와 연결
    - 페이지 데이터를 ViewModel을 통해 관리, 마지막 페이지에 별도 Fragment를 표시. (FairyTaleDetailLastFragment)
    - 마지막 페이지 제외 페이지 리스트를 기반으로 각 페이지에 맞는 Fragment 생성 (FairyTalePageFragment)

---
+++ app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/FairyTalePageAdapter.kt
@@ -0,0 +1,32 @@
+package com.sangik.iluvbook.fairytale.detail.ui
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sangik.iluvbook.fairytale.detail.viewmodel.FairyTaleDetailViewModel
+
+//ViewPager2의 각 페이지 관리 어댑터
+class FairyTalePageAdapter(
+    fragment : Fragment,
+    private val vm : FairyTaleDetailViewModel
+    ) : FragmentStateAdapter(fragment) {
+
+    // ViewPager의 총 페이지 수를 반환
+    override fun getItemCount(): Int {
+        // FairyTaleDetailLastPage 를 위한 마지막 페이지 1개 더 추가
+        return vm.pages.value?.size?.plus(1) ?: 0
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val pageCount = vm.pages.value?.size ?: 0
+
+        return if (position < pageCount) {
+            // 현재 position에 해당하는 Page 데이터를 가져와 FairyTalePageFragment를 생성
+            val pageData = vm.pages.value?.get(position)
+            FairyTalePageFragment.newInstance(pageData)
+        } else {
+            // 마지막 페이지일 경우 FairyTaleDetailLastPageFragment를 반환
+            FairyTaleDetailLastPageFragment()
+        }
+    }
+
+}
