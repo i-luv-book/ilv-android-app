bf0b44e42617cd18ffc480209a4ca2fa55422463
김상익 <ksik518@naver.com>
2024-09-25 21:40:52 +0900

    [ILV-68] 하단 Bottom Navigation 적용 완료
    Selector를 사용한 색상 변경
    각 아이템 title 글꼴 변경
    nav_graph에 각 item Fragment 추가
    menu_click_status_color는 res/color로 이동

--- app/src/main/java/com/sangik/iluvbook/main/ui/MainActivity.kt
+++ app/src/main/java/com/sangik/iluvbook/main/ui/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.sangik.iluvbook.main.ui
 
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sangik.iluvbook.BR
 import com.sangik.iluvbook.R
 import com.sangik.iluvbook.base.BaseActivity
 import com.sangik.iluvbook.databinding.ActivityMainBinding
-import com.sangik.iluvbook.util.ActivityUtil
 import com.sangik.iluvbook.main.viewmodel.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
     R.layout.activity_main,
     MainViewModel::class,
     BR.mainViewModel
-) {}
+) {
+    override fun onStart() {
+        super.onStart()
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+}
