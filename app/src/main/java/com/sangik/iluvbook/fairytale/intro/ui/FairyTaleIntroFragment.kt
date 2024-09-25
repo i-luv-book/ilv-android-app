f4f08e5940ca22ff8e538cce383f5d27301c42fa
김상익 <ksik518@naver.com>
2024-09-25 14:41:33 +0900

    [ILV-67] 동화 상세 이동 클릭 리스너 설정
    safeArgs를 통한 데이터 전달 및 이동

--- app/src/main/java/com/sangik/iluvbook/fairytale/intro/ui/FairyTaleIntroFragment.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/intro/ui/FairyTaleIntroFragment.kt
@@ -1,12 +1,14 @@
 package com.sangik.iluvbook.fairytale.intro.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -43,7 +45,23 @@ class FairyTaleIntroFragment : Fragment() {
         // RecyclerView 설정
         setRecyclerView(args.keywords.toList())
 
+        // 썸네일 이미지 설정
         setGlideImage()
+
+        initListener()
+    }
+
+    private fun initListener() {
+        // 동화 상세로 navigate
+        actionToFairyTaleDetail()
+    }
+
+    private fun actionToFairyTaleDetail() {
+        binding.btnReadFairytale.setOnClickListener {
+            val actionToFairyTaleDetail = FairyTaleIntroFragmentDirections
+                .actionFairyTaleIntroFragmentToFairyTaleDetailFragment(args.fairyTaleResponse)
+            findNavController().navigate(actionToFairyTaleDetail)
+        }
     }
 
     private fun setGlideImage() {
