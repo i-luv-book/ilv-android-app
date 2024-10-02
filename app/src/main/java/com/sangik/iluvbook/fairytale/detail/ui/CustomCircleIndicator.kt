482053ddb01dfa10fd5adbb1ffd9d76a32c66934
김상익 <ksik518@naver.com>
2024-10-02 22:14:09 +0900

    [ILV-70] feat : 선택형 동화 Indicator 변경
    선택형 동화 1,3, 5 번째는 별모양 Indicator로 설정
    Keywords Parcelable 추가 (safe args 사용)

--- app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/CustomCircleIndicator.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/CustomCircleIndicator.kt
@@ -7,14 +7,20 @@ import com.sangik.iluvbook.R
 import me.relex.circleindicator.CircleIndicator3
 
 class CustomCircleIndicator(context: Context, attrs: AttributeSet?) : CircleIndicator3(context, attrs) {
+    private var isPremium: Boolean = false  // 선택형 동화 설정 플래그
+
     override fun setViewPager(viewPager: ViewPager2?) {
         super.setViewPager(viewPager)
         updateIndicatorState(viewPager?.currentItem ?: 0)
     }
 
+    fun setIsPremium(isPremium : Boolean) {
+        this.isPremium = isPremium
+    }
+
     internal fun updateIndicatorState(position: Int) {
         for (i in 0 until childCount) {
-            updateIndicator(i, i == position)  // 선택된 인디케이터는 true, 나머지는 false로 업데이트
+            updateIndicator(i, i == position)  // 선택된 인디케이터는 true, 나머지는 false
         }
     }
 
@@ -24,10 +30,21 @@ class CustomCircleIndicator(context: Context, attrs: AttributeSet?) : CircleIndi
         val layoutParams = indicator.layoutParams as MarginLayoutParams
 
         val (width, height, backgroundRes) = if (isSelected) {
-            Triple(dpToPx(22), dpToPx(10), R.drawable.indicator_selected)
+            // 선택된 인디케이터
+            if (isPremium && (position == 1 || position == 3 || position == 5)) {
+                Triple(dpToPx(12), dpToPx(10), R.drawable.indicator_star_active) // 선택형 동화
+            } else {
+                Triple(dpToPx(22), dpToPx(10), R.drawable.indicator_selected) // 일반 동화
+            }
         } else {
-            Triple(dpToPx(12), dpToPx(12), R.drawable.indicator_unselected)
+            // 선택되지 않은 인디케이터
+            if (isPremium && (position == 2 || position == 4 || position == 6)) {
+                Triple(dpToPx(12), dpToPx(10), R.drawable.indicator_star_unselected) // 선택형 동화
+            } else {
+                Triple(dpToPx(12), dpToPx(12), R.drawable.indicator_unselected) // 일반 동화
+            }
         }
+
 
         indicator.setBackgroundResource(backgroundRes)
         layoutParams.setMargins(dpToPx(8), 0, dpToPx(8), 0)
--- app/src/main/java/com/sangik/iluvbook/fairytale/model/dto/Keywords.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/model/dto/Keywords.kt
@@ -1,8 +1,13 @@
 package com.sangik.iluvbook.fairytale.model.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Keywords(
     val traits: List<String>, // 주인공
     val characters: List<String>, // 주인공 이름
     val settings: List<String>, // 배경
     val genre: List<String> // 장르
-)
+) : Parcelable
+
