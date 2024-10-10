ecc2b2bf4a9ec57b250eb790d837096f22042b77
김상익 <ksik518@naver.com>
2024-10-10 22:50:10 +0900

    [ILV-70] feat : feat: CustomCircleIndicator에 ViewModel 연동 및 인디케이터 상태 업데이트 로직 개선
    - CustomCircleIndicator : FairyTaleDetailViewModel을 사용
    - updateIndicatorState : indicator 상태 업데이트
    - 페이지 타입 기반으로 indicator 상태를 구분

--- app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/CustomCircleIndicator.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/CustomCircleIndicator.kt
@@ -2,49 +2,60 @@ package com.sangik.iluvbook.fairytale.detail.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.lifecycle.LifecycleOwner
 import androidx.viewpager2.widget.ViewPager2
 import com.sangik.iluvbook.R
+import com.sangik.iluvbook.fairytale.detail.viewmodel.FairyTaleDetailViewModel
+import com.sangik.iluvbook.fairytale.model.dto.PageData
 import me.relex.circleindicator.CircleIndicator3
 
 class CustomCircleIndicator(context: Context, attrs: AttributeSet?) : CircleIndicator3(context, attrs) {
-    private var isPremium: Boolean = false  // 선택형 동화 설정 플래그
+
+    // ViewModel, CustomCircleIndicator 연결, 페이지 상태 변화 감지
+    fun bindViewModel(viewModel : FairyTaleDetailViewModel) {
+        viewModel.currentPageType.observe((context as? LifecycleOwner)?: return) { pageData ->
+            // 현재 페이지 타입 관찰
+            updateIndicatorState(viewModel, viewModel.getCurrentPageIndex())
+        }
+
+        viewModel.currentPageIndex.observe((context as? LifecycleOwner) ?: return) { position ->
+            updateIndicatorState(viewModel, position)
+        }
+    }
 
     override fun setViewPager(viewPager: ViewPager2?) {
         super.setViewPager(viewPager)
-        updateIndicatorState(viewPager?.currentItem ?: 0)
+        updateIndicatorState(null, viewPager?.currentItem ?: 0)
     }
 
-    fun setIsPremium(isPremium : Boolean) {
-        this.isPremium = isPremium
-    }
-
-    internal fun updateIndicatorState(position: Int) {
+    // 인디케이터 상태 업데이트 함수
+    internal fun updateIndicatorState(viewModel: FairyTaleDetailViewModel?, position: Int) {
         for (i in 0 until childCount) {
-            updateIndicator(i, i == position)  // 선택된 인디케이터는 true, 나머지는 false
+            val isSelectionPage = viewModel?.getPageDataAt(i) is PageData.SelectionPage
+            updateIndicator(i, i == position, isSelectionPage)
+
         }
     }
 
+
     // Indicator UI 업데이트
-    private fun updateIndicator(position: Int, isSelected: Boolean) {
+    private fun updateIndicator(position: Int, isSelected: Boolean, isSelectionPage : Boolean) {
         val indicator = getChildAt(position)
         val layoutParams = indicator.layoutParams as MarginLayoutParams
 
-        val (width, height, backgroundRes) = if (isSelected) {
-            // 선택된 인디케이터
-            if (isPremium && (position == 1 || position == 3 || position == 5)) {
+        val (width, height, backgroundRes) = if (isSelected) { // 현재 보고있는 페이지 해당 Indicator
+            if (isSelectionPage) { // 옵션 선택 페이지인 경우
                 Triple(dpToPx(12), dpToPx(10), R.drawable.indicator_star_active) // 선택형 동화
-            } else {
+            }else{ // 일반 동화 페이지인 경우
                 Triple(dpToPx(22), dpToPx(10), R.drawable.indicator_selected) // 일반 동화
             }
         } else {
-            // 선택되지 않은 인디케이터
-            if (isPremium && (position == 2 || position == 4 || position == 6)) {
-                Triple(dpToPx(12), dpToPx(10), R.drawable.indicator_star_unselected) // 선택형 동화
+            if (isSelectionPage) {
+                Triple(dpToPx(15), dpToPx(15), R.drawable.indicator_star_unselected)
             } else {
-                Triple(dpToPx(12), dpToPx(12), R.drawable.indicator_unselected) // 일반 동화
+                Triple(dpToPx(12), dpToPx(12), R.drawable.indicator_unselected)
             }
         }
-
 
         indicator.setBackgroundResource(backgroundRes)
         layoutParams.setMargins(dpToPx(8), 0, dpToPx(8), 0)
@@ -52,7 +63,6 @@ class CustomCircleIndicator(context: Context, attrs: AttributeSet?) : CircleIndi
         layoutParams.height = height
         indicator.layoutParams = layoutParams
     }
-
 
     // dp -> px 변환 함수
     private fun dpToPx(dp: Int): Int {
