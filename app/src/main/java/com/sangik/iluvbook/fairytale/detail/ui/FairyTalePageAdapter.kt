be68f6bf6e5e5b428cc3ccae3e18b9d0fe67161b
김상익 <ksik518@naver.com>
2024-10-13 23:44:44 +0900

    [ILV-70] feat : 선태형 동화 기능 추가 : FairyTaleAdapter 수정
    - 선택형 동화, 마지막 페이지 처리하도록 변경
    - 각 페이지 유형에 따라 적절한 Fragment 반환
    - getItemId, containsItem 메서드 재정의 : 변경된 데이터 동적 아이템 변경

--- app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/FairyTalePageAdapter.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/detail/ui/FairyTalePageAdapter.kt
@@ -3,31 +3,53 @@ package com.sangik.iluvbook.fairytale.detail.ui
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sangik.iluvbook.fairytale.detail.viewmodel.FairyTaleDetailViewModel
+import com.sangik.iluvbook.fairytale.model.dto.PageData
 
-//ViewPager2의 각 페이지 관리 어댑터
 class FairyTalePageAdapter(
     fragment : Fragment,
-    private val vm : FairyTaleDetailViewModel
+    private val fairyTaleDetailViewModel: FairyTaleDetailViewModel,
     ) : FragmentStateAdapter(fragment) {
 
-    // ViewPager의 총 페이지 수를 반환
     override fun getItemCount(): Int {
-        // FairyTaleDetailLastPage 를 위한 마지막 페이지 1개 더 추가
-        return vm.pages.value?.size?.plus(1) ?: 0
+        return fairyTaleDetailViewModel.getTotalPageCount()
     }
 
     override fun createFragment(position: Int): Fragment {
-        val pageCount = vm.pages.value?.size ?: 0
+        return when (val pageData = fairyTaleDetailViewModel.getPageDataAt(position)) {
+            is PageData.RegularPage -> {
+                FairyTalePageFragment.newInstance(
+                    fairyTaleTitle = pageData.title,
+                    fairyTaleContent = pageData.content,
+                    imgUrl = pageData.imgUrl.orEmpty()
+                )
+            }
+            // 선택형 동화
+            is PageData.SelectionPage -> {
+                FairyTaleSelectionFragment.newInstance(pageData.options)
+            }
 
-        val fairyTaleTitle = vm.fairyTaleTitle.value.toString()
-        return if (position < pageCount) {
-            // 현재 position에 해당하는 Page 데이터를 가져와 FairyTalePageFragment를 생성
-            val pageData = vm.pages.value?.get(position)
-            FairyTalePageFragment.newInstance(pageData, fairyTaleTitle)
-        } else {
-            // 마지막 페이지일 경우 FairyTaleDetailLastPageFragment를 반환
-            FairyTaleDetailLastPageFragment.newInstance(fairyTaleTitle)
+            // 마지막 페이지
+            is PageData.LastPage -> {
+                FairyTaleDetailLastPageFragment.newInstance(
+                    fairyTaleTitle = pageData.title
+                )
+            }
+            else -> {
+                Fragment()
+            }
         }
+    }
+
+    override fun getItemId(position: Int): Long {
+        // 해당 데이터 고유 ID
+        return fairyTaleDetailViewModel.getPageDataAt(position)?.hashCode()?.toLong() ?: position.toLong()
+    }
+
+    // 특정 ID 페이지의 포함 여부
+    override fun containsItem(itemId: Long): Boolean {
+        return fairyTaleDetailViewModel.pageDataList.value?.any {
+            it.hashCode().toLong() == itemId
+        } ?: false
     }
 
 }
