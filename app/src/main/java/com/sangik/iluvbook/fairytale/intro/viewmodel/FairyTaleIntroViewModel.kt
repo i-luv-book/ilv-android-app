e9471d6b0265a1b86d8c9fb3323f2083f2fd0b02
김상익 <ksik518@naver.com>
2024-10-15 16:08:33 +0900

    [ILV-70] feat : "FairyTaleIntroFragment, ViewModel 수정
    - FairyTaleIntroViewModel : initData 함수 수정 FairyTaleResponse 기반에서 개별 데이터 필드 설정
    - fairyTaleResponse 와 fairyTaleSelectionResponse를 관찰하도록 수정.
    - 선택형 동화 추가에 따른, Glide 이미지 처리 범용성 있도록 변경

--- app/src/main/java/com/sangik/iluvbook/fairytale/intro/viewmodel/FairyTaleIntroViewModel.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/intro/viewmodel/FairyTaleIntroViewModel.kt
@@ -4,20 +4,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.sangik.iluvbook.base.BaseViewModel
 import com.sangik.iluvbook.fairytale.model.dto.FairyTaleResponse
+import com.sangik.iluvbook.fairytale.model.dto.FairyTaleSelectionResponse
 
 class FairyTaleIntroViewModel : BaseViewModel() {
     // 생성된 동화
 
-    private val _fairyTaleResponse = MutableLiveData<FairyTaleResponse>()
-    val fairyTaleResponse: LiveData<FairyTaleResponse> get() = _fairyTaleResponse
+    private val _fairyTaleImgUrl = MutableLiveData<String>()
+    val fairyTaleImgUrl : LiveData<String> get() =  _fairyTaleImgUrl
 
+
+    private val _fairyTaleTitle = MutableLiveData<String>()
+    val fairyTaleTitle : LiveData<String> get() = _fairyTaleTitle
+
+    private val _fairyTaleSummary = MutableLiveData<String>()
+    val fairyTaleSummary : LiveData<String> get() = _fairyTaleSummary
 
     // 사용자 선택 모든 키워드
     private val _keywords = MutableLiveData<List<String>>()
     val keywords: LiveData<List<String>> get() = _keywords
 
-    fun initData(response: FairyTaleResponse, keywords : List<String>) {
-        _fairyTaleResponse.value = response
-        _keywords.value = keywords
+    fun initData(fairyTaleTitle : String, fairyTaleSummary : String, imgUrl : String) {
+        _fairyTaleTitle.value = fairyTaleTitle
+        _fairyTaleSummary.value = fairyTaleSummary
+        _fairyTaleImgUrl.value = imgUrl
     }
 }
