0c83c16af61bd9016535800a5aa026929c26c36f
김상익 <ksik518@naver.com>
2024-10-15 16:00:05 +0900

    [ILV-70] feat : 마지막 선택형 동화 호출 추가
    선택형 동화 마지막 호출 API Repository, Service 클래스 추가

--- app/src/main/java/com/sangik/iluvbook/network/service/FairyTaleLastService.kt
+++ app/src/main/java/com/sangik/iluvbook/network/service/FairyTaleLastService.kt
@@ -1,15 +1,15 @@
 package com.sangik.iluvbook.network.service
 
-import com.sangik.iluvbook.fairytale.model.dto.FairyTaleEndResponse
+import com.sangik.iluvbook.fairytale.model.dto.FairyTaleLastResponse
 import com.sangik.iluvbook.fairytale.model.dto.FairyTaleSelectionRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface FairyTaleEndService {
+interface FairyTaleLastService {
     // 선택형 동화 마지막 호출
     @POST("fairytale/game/end")
-    suspend fun createFairyTaleEnd(
+    suspend fun createFairyTaleLast(
         @Body request : FairyTaleSelectionRequest
-    ) : Response<FairyTaleEndResponse>
+    ) : Response<FairyTaleLastResponse>
 }
