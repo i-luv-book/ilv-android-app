586b677efb3859e89e270c880a0bb6414df62cb8
김상익 <ksik518@naver.com>
2024-11-13 16:30:30 +0900

    [ILVA-77] feat: 퀴즈 패키지 분리
    - 퀴즈를 main, solve 패키지로 분리
    - 기존 QuizFragment는 QuizMainFragment로 변경

--- app/src/main/java/com/sangik/iluvbook/quiz/main/ui/QuizMainFragment.kt
+++ app/src/main/java/com/sangik/iluvbook/quiz/main/ui/QuizMainFragment.kt
@@ -1,4 +1,4 @@
-package com.sangik.iluvbook.quiz.ui
+package com.sangik.iluvbook.quiz.main.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.sangik.iluvbook.R
 
-class QuizFragment : Fragment() {
+class QuizMainFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        return inflater.inflate(R.layout.fragment_quiz_main, container, false)
     }
 }
