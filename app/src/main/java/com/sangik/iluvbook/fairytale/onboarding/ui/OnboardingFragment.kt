90b354859c860a411b08d5dbffd3ec3e81ddb778
김상익 <ksik518@naver.com>
2024-12-20 23:30:56 +0900

    [ILVA-81] refactor: BaseFragment 도입
    - OnboardingFragment : BaseFragment 도입

--- app/src/main/java/com/sangik/iluvbook/fairytale/onboarding/ui/OnboardingFragment.kt
+++ app/src/main/java/com/sangik/iluvbook/fairytale/onboarding/ui/OnboardingFragment.kt
@@ -1,43 +1,17 @@
 package com.sangik.iluvbook.fairytale.onboarding.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.sangik.iluvbook.R
+import com.sangik.iluvbook.base.BaseFragment
 import com.sangik.iluvbook.databinding.FragmentOnboardingBinding
 import com.sangik.iluvbook.fairytale.onboarding.viewmodel.OnboardingViewModel
 
-class OnboardingFragment : Fragment() {
-    private lateinit var vm : OnboardingViewModel
+class OnboardingFragment : BaseFragment<FragmentOnboardingBinding, OnboardingViewModel>(
+    R.layout.fragment_onboarding,
+    OnboardingViewModel::class
+) {
 
-    private lateinit var binding : FragmentOnboardingBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        vm = ViewModelProvider(requireActivity()).get(OnboardingViewModel::class.java)
-
-        binding = DataBindingUtil.inflate<FragmentOnboardingBinding?>(
-            inflater, R.layout.fragment_onboarding, container, false
-        ).apply {
-            lifecycleOwner = viewLifecycleOwner
-            onboardingViewModel = vm
-        }
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initListener()
-    }
-
-    private fun initListener() {
+    override fun initListener() {
         with(binding) {
             btnLow.setOnClickListener{ navigateToFairyTaleCreationFragment("low") } // 난이도 선택 : 영유아
             btnMid.setOnClickListener{ navigateToFairyTaleCreationFragment("mid") } // 난이도 선택 : 초등학교 저학년
@@ -49,5 +23,4 @@ class OnboardingFragment : Fragment() {
             .actionOnboardingFragmentToFairyTaleCreationFragment(selectedLevel)
         findNavController().navigate(actionToFairyTaleCreation)
     }
-
 }
